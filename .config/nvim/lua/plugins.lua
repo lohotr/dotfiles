1f5d9c34cc0de40f53467d48e77087671a552498
Ron <thlong.work@gmail.com>
2023-06-20 13:32:43 +0000

    Install package manager and add theme

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,33 @@
+-- Install package manager
+--    https://github.com/folke/lazy.nvim
+--    `:help lazy.nvim.txt` for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system { -- clone lazy.nvim from git registry
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- NOTE: Here is where you install your plugins.
+--  You can configure plugins using the `config` key.
+--
+--  You can also configure plugins after the setup call,
+--    as they will be available in your neovim runtime.
+require('lazy').setup({
+  {
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    config = function()
+      require('onedark').setup {
+        style = 'deep'
+      }
+      vim.cmd.colorscheme 'onedark'
+    end,
+  },
+}, {})
