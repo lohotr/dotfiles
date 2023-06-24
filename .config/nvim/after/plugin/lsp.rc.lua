bfed9680bb75cbc686da40801712d69707c7a432
Ron <thlong.work@gmail.com>
2023-06-24 02:06:00 +0000

    Add nvim-cmp plugin

--- .config/nvim/after/plugin/lsp.rc.lua
+++ .config/nvim/after/plugin/lsp.rc.lua
@@ -65,6 +65,12 @@ end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
+-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+local ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
+if ok then
+  capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+end
+
 lspconfig.tsserver.setup({
   capabilities = capabilities,
   on_attach = on_attach,
