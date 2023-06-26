f263b145305959d58fa22550029b138114c75a09
Ron <thlong.work@gmail.com>
2023-06-26 11:22:58 +0700

    Shorten path for telescope

--- .config/nvim/after/plugin/telescope.rc.lua
+++ .config/nvim/after/plugin/telescope.rc.lua
@@ -1,5 +1,11 @@
 local ok, telescope = pcall(require, 'telescope')
 if (not ok) then return end
 
+telescope.setup({
+  defaults = {
+    path_display = { 'smart' } -- shorten path when find or search files
+  }
+})
+
 -- To get fzf loaded and working with telescope
 telescope.load_extension('fzf')
